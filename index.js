21418285b47f5805affb4ab97ae92f39d363c483
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-08 15:17:28 +0600

    Added: SErver side

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+const PORT = process.env.port || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
