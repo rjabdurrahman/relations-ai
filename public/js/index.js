b16f3c5c469ae3b315537d7b79319e179fb74833
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-20 21:09:32 +0600

    Fixed: Mobile video toggle

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,25 @@ app.controller('myCtrl', function ($scope) {
         return $scope.videoMode ? 'messenger_' : 'whatsapp_';
     }
     $scope.dashboardImg = 'analytics.jpg';
-    $('.mobile-wa-carousel').on('changed.owl.carousel', function(event) {
-        console.log('Fired');
+    $('.mobile-wa-carousel').on('changed.owl.carousel', function (e) {
+        let currentIndex = e.item.index;
+        switch(currentIndex) {
+            case 2: $scope.video = 'go_personal';
+            break;
+            case 3: $scope.video = 'super_app';
+            break;
+            case 4: $scope.video = '11x_sales';
+            break;
+            case 5: $scope.video = 'highest_open_rate';
+            break;
+            case 6: $scope.video = 'go_personal';
+            break;
+        }
+        $scope.$applyAsync();
     });
 
-    $('.dashboard-carousel').on('changed.owl.carousel', function(event) {
+    $('.dashboard-carousel').on('changed.owl.carousel', function (e) {
+        // console.log("current: ",e.item.index) //same
         console.log('Fired2');
     });
 });
